73c0fe6570a8f6a9e16e831275ba719403b99113
JULIET NGOZI EZE <104633983+julietngozi@users.noreply.github.com>
2026-05-29 06:34:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBtn =
+document.getElementById("menuBtn");
+
+const navMenu =
+document.getElementById("navMenu");
+
+menuBtn.onclick = function(){
+
+  navMenu.classList.toggle("active");
+
+};
